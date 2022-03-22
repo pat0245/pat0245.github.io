a3f49691f9cbeacae16e155cce259b787cd989c1
pat0245 <91133345+pat0245@users.noreply.github.com>
2022-03-22 13:15:23 +0100

    prototype of "Box" and First concept of Index

--- pages/about.jsx
+++ pages/about.jsx
@@ -16,6 +16,33 @@ export default function About() {
 
       <Container className={styles.container}>
         <h1>About</h1>
+        <p>
+          My nmae is Patrick Maier and I am here since 2005, to be more preciese
+          the 2nd april 2005
+        </p>
+        <h3>Movies, Games etc I've enjoyed!</h3>
+        <ul>
+          <li>
+            <a href="#" className={styles.list}>
+              Games
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.list}>
+              Movies
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.list}>
+              Books
+            </a>
+          </li>
+          <li>
+            <a href="#" className={styles.list}>
+              TV
+            </a>
+          </li>
+        </ul>
       </Container>
     </>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Container from "../components/Container";
 import Navigation from "../components/Navigation";
 import Head from "next/head";
 import styles from "../styles/index.module.css";
+import Box from "../components/Box";
 
 export default function Home() {
   return (
@@ -12,20 +13,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        <Navigation />     
+      <Navigation />
 
       <Container className={styles.container}>
         <h1>Hello</h1>
         <p>and welcome to my website!</p>
         <ul>
           <li>
-            <a href="#" className={styles.linkList}>
+            <a href="/about" className={styles.linkList}>
               more about me
             </a>
           </li>
         </ul>
         <h1>Blog</h1>
-        <p>in this block I will tell you something about my life and stuff</p>
+        <p>in this block I will tell you something about my life and stuff.</p>
+        <div className={styles.box}>
+          <a href="#">
+            <Box title="More about me" body="I was born..." />
+          </a>
+        </div>
+        <h1>Special Thanks!</h1>
+        <p>to the person who thought me how to programm and indpired this website:</p>
+        <a href="https/valwal.com">Valentin Walter</a>
       </Container>
     </>
   );
