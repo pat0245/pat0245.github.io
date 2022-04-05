ccccd8b37aaecd80f2c086e264f5c7819e166f02
pat0245 <91133345+pat0245@users.noreply.github.com>
2022-04-05 18:38:45 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  assetPrefix: isProd ? "/pat0245.github.io/" : "",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
