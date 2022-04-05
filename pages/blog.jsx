7b45052a1915c5c3dff5537e0ae27871e677b8a8
pat0245 <91133345+pat0245@users.noreply.github.com>
2022-04-05 18:24:42 +0200

    fix bug (Cockroach)

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -3,26 +3,30 @@ import Navigation from "../components/Navigation";
 import Head from "next/head";
 import styles from "../styles/blog.module.css";
 import Box from "../components/Box";
+import Link from "next/link"
 
 export default function Blog() {
   return (
     <>
       <Head>
         <title>Pat | About</title>
-        <meta name="description" content="Patrick Maier&apos;s website" />
+        <meta name="description" content="Patrick Maier's website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Navigation />
       <Container classname={styles.container}>
         <h1>Blog</h1>
-
-        <a href="#">
-          <Box color="lightblue" title="test" body="test" />
-        </a>
-        <a href="#">
-          <Box color="lightblue" title="test2" body="test2" />
-        </a>
+        <Link href="#">
+          <a>
+            <Box color="lightblue" title="test" body="test" />
+          </a>
+        </Link>
+        <Link href="#">
+          <a>
+            <Box color="lightblue" title="test2" body="test2" />
+          </a>
+        </Link>
       </Container>
     </>
   );
