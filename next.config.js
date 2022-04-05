36f2012ccea1d593fbfdb492a29890edef0dc7e8
pat0245 <91133345+pat0245@users.noreply.github.com>
2022-04-05 18:15:24 +0200

    fix bug

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath:"/docs"
-}
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: isProd ? "/pat0245.github.io/" : "",
+};
