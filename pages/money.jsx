261e936848878fb9de4c4a8f023ffc195292d09e
pat0245 <91133345+pat0245@users.noreply.github.com>
2022-08-16 12:32:28 +0100

    Create money.jsx

---
+++ pages/money.jsx
@@ -0,0 +1,21 @@
+import Container from "../components/Container";
+import Navigation from "../components/Navigation";
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Inspiration() {
+  return (
+    <>
+      <Head>
+        <title>Pat | Human</title>
+        <meta name="description" content="Patrick Maier's website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navigation />
+      <Container className="p-24">
+       
+      </Container>
+    </>
+  );
+}
